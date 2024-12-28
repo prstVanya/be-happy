eeee199344794eb5bc5a027a44d847c46d1ef66e
Ivan <vany4golos@yandex.ru>
2024-12-28 13:01:36 +0100

    test

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/be-happy/reactjs-template',
+  base: '/be-happy/',
   plugins: [
     // Allows using React dev server along with building a React application with Vite.
     // https://npmjs.com/package/@vitejs/plugin-react-swc
